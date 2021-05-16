90474b682fedfbd4136baa3d3efacbd28e387bee
Christoph Buelter <buelter.christoph@gmail.com>
2021-05-16 11:15:44 +0200

    Fix scoping of service-worker.js

--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 
 from server.catflap import views
@@ -22,5 +23,17 @@ urlpatterns = [
         "catflaps/<uuid:catflap_uuid>/status",
         views.get_catflap_status,
         name="status",
+    ),
+    # This endpoint serves a service-worker file on root level, so it
+    # can control anything below. Why are we using a template here
+    # instead of service a static file? Because the file needs to be
+    # hosted above the /static/ path for proper scoping.
+    path(
+        r"service-worker",
+        TemplateView.as_view(
+            template_name=("service-worker.js"),
+            content_type="application/javascript",
+        ),
+        name="service-worker",
     ),
 ]
