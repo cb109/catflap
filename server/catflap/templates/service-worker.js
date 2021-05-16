f7ed1d99e1494a39702ca54315a4958dac42a76a
Christoph Buelter <buelter.christoph@gmail.com>
2021-05-16 11:40:56 +0200

    Bundle CDN libs so we can cache them with the service-worker

--- server/catflap/templates/service-worker.js
+++ server/catflap/templates/service-worker.js
@@ -2,12 +2,22 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('catflap').then(function(cache){
       cache.addAll([
-
+        '/static/apexcharts.3.26.1.min.js',
+        '/static/bulma.0.9.2.min.css',
+        '/static/dayjs.1.8.28.min.js',
+        '/static/icon-144x144.png',
+        '/static/icon-192x192.png',
+        '/static/icon-72x72.png',
+        '/static/icon-96x96.png',
       ]);
     })
   );
 });
 
 self.addEventListener('fetch', function(e) {
-
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
